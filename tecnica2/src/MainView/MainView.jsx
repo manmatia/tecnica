053d68e74fda9d79e5d1e0b664ad4a50e727bb19
Matias Graneros <manmatia@gmail.com>
2024-09-27 15:33:47 -0300

    change words

--- tecnica2/src/MainView/MainView.jsx
+++ tecnica2/src/MainView/MainView.jsx
@@ -9,18 +9,18 @@ function MainView() {
                     <div className="">
                         <div className="card-body " style={{ marginTop: '12%' }}>
                             <div className="d-flex">
-                                <button style={{ color: 'white', margin: "2%", marginLeft: "15%" }} className="custom-button4 text" type="submit"> Welcome to Businezz x</button>
+                                <button style={{ color: 'white', margin: "2%", marginLeft: "15%" }} className="custom-button4 text" type="submit"> Welcome to Businezz X</button>
                             </div>
 
-                            <h2 className="card-title" style={{ textAlign: 'left', color: 'white', marginLeft: "15%" }}>
+                            <h1 className="card-title" style={{ textAlign: 'left', color: 'white', marginLeft: "15%" }}>
                                 Join us in growing <br /> your <span className="span2">business.</span>
-                            </h2>
+                            </h1>
                         </div>
                         <hr style={{ borderColor: '#00bfff', borderWidth: '1px', marginLeft: "15%", marginRight:"15%", marginTop:"18%"}} />
                         <div className="card-body">
                             <h4 className="card-title mt-2" style={{ textAlign: 'left', color: 'white', marginLeft: "15%", }}>
-                                Elevate your business with Businezz x,<br />
-                                a professional Webflow template.
+                                Elevate your business with Businezz X, a<br />
+                                 professional Webflow template.
                             </h4>
                             <div className="d-flex">
                                 <button style={{ color: 'white', margin: "2%", marginLeft: "15%" }} className="custom-button text" type="submit">Contact us</button>
@@ -65,7 +65,7 @@ function MainView() {
                                         type="email"
                                         className="form-control text"
                                         id="email"
-                                        placeholder="Contact@mail.com"
+                                        placeholder="Contact@email.com"
                                         required
                                     />
                                 </div>
@@ -76,7 +76,7 @@ function MainView() {
                                     className="form-control"
                                     id="message"
                                     rows="3"
-                                    placeholder="Your message"
+                                    placeholder="Pease type your message here..."
                                     required
                                 />
                             </div>
@@ -86,7 +86,7 @@ function MainView() {
                                 className="custom-button2"
                                 style={{ marginTop: '20px' }}
                             >
-                                Get in touch
+                                Get in Touch
                             </button>
                         </form>
                     </div>
--- tecnica2/src/NavBar/Navbar.jsx
+++ tecnica2/src/NavBar/Navbar.jsx
@@ -2,44 +2,52 @@ import './NavBar.css';
 
 function NavBar() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg bg-white mt20">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#"><img style={{ height: '50px', width: '50px' }} src="https://logopond.com/logos/7e0167b70fffba2638bc6f5984af1646.png"></img></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active text" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text" href="#">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text" href="#">Pages⬇
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text" href="#">Packages</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text" href="#">Contact</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text" href="#">Cart[0]</a>
-                </li>
-              </ul>
-              <div className="d-flex">
-              <button className="custom-button text" type="submit">Free Consultation ➡️</button>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
+        <div>
+            <nav className="navbar navbar-expand-lg bg-white mt20" style={{ color: 'white', marginLeft: "6%" }}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img style={{ height: '50px', width: '50px' }} src="https://logopond.com/logos/7e0167b70fffba2638bc6f5984af1646.png" alt="Logo" />
+                    </a>
+                    {/* Línea vertical */}
+                    <div style={{
+                        width: '2px', 
+                        backgroundColor: '#E7E7E7', 
+                        height: '40px',
+                        marginLeft: '10px', 
+                        marginRight: '10px' 
+                    }}></div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active text" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item" >
+                                <a className="nav-link text" href="#" style={{marginRight:"1.5rem" , marginLeft:"1.5rem"}}>About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text" href="#" style={{marginRight:"1.5rem" , marginLeft:"1.5rem"}}>Pages ⬇</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text" href="#" style={{marginRight:"1.5rem" , marginLeft:"1.5rem"}}>Packages</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text" href="#" style={{marginRight:"1.5rem" , marginLeft:"1.5rem"}}>Contact</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text" href="#" style={{marginRight:"1.5rem" , marginLeft:"1.5rem"}}>Cart[0]</a>
+                            </li>
+                        </ul>
+                        <div className="d-flex"style={{marginRight:"15%"}}>
+                            <button className="custom-button text" type="submit">Free Consultation ➡️</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
     );
-  }
-  
-  export default NavBar;
-  
+}
+
+export default NavBar;
